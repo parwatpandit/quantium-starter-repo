f4f8fc2bcd76b63e6fa7b2068e62fd1129330666
parwatpandit <parbatp777@gmail.com>
2026-05-18 21:41:24 +0100

    writing code to  show the total sales and spliting the sels accordingly and the creating a new output.csv file to show the result as requested

--- app.py
+++ app.py
@@ -18,5 +18,29 @@ df_pink.to_csv("pink_morsel.csv", index=False)
 pink_morsel=pd.read_csv("pink_morsel.csv")
 
 pink_morsel["price"] = pink_morsel["price"].str.replace("$", "").astype(float)
+# print(pink_morsel["price"])
 
-print(pink_morsel["price"])
+# calculating sales totoal
+pink_morsel["sales"] = pink_morsel["price"] * pink_morsel["quantity"]
+# print(pink_morsel["sales"])
+print("total Sales:", pink_morsel["sales"].sum())
+
+
+
+# spliting the salsed before and after 15th Jand 2021
+
+before = pink_morsel[pink_morsel["date"] < "2021-01-15"]
+after = pink_morsel[pink_morsel["date"] >= "2021-01-15"]
+
+print("Before:", before["sales"].sum())
+print("After:", after["sales"].sum())
+
+
+# outpus the before and after now
+output = pd.DataFrame({
+    "sales": [before["sales"].sum(), after["sales"].sum()],
+    "date": ["before 15th Jan 2021", "after 15th Jan 2021"]
+})
+
+output.to_csv("output.csv", index=False)
+print(output)
