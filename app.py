6f907eea1757235f287214f0664ea58fb4692e10
parwatpandit <parbatp777@gmail.com>
2026-05-18 20:51:54 +0100

    creating app.py for analysing tefiles and and usign app.py to writing code to seperate the pink_morsel form al 7 products in seperate .csv file whic is pink_morsel.csv

---
+++ app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Step 1 - Read all 3 CSV files
+df1 = pd.read_csv("data/daily_sales_data_0.csv")
+df2 = pd.read_csv("data/daily_sales_data_1.csv")
+df3 = pd.read_csv("data/daily_sales_data_2.csv")
+
+
+df = pd.concat([df1, df2, df3])
+df_pink=(df[df["product"]=="pink morsel"])
+df_pink.to_csv("pink_morsel.csv", index=False)
+
+
+# # Step 5 - Save to new CSV
+# df.to_csv("result.csv",index=False)
+# print("Done!")
