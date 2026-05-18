191f74a889f2db4b8fde96d0b75a89b21d0eb74f
parwatpandit <parbatp777@gmail.com>
2026-05-18 20:56:01 +0100

    modifying app.py  if pink_morsel.csv exist then do noting syntax using os and also imported os

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import os
 # Step 1 - Read all 3 CSV files
 df1 = pd.read_csv("data/daily_sales_data_0.csv")
 df2 = pd.read_csv("data/daily_sales_data_1.csv")
@@ -8,6 +8,9 @@ df3 = pd.read_csv("data/daily_sales_data_2.csv")
 
 df = pd.concat([df1, df2, df3])
 df_pink=(df[df["product"]=="pink morsel"])
+
+if not os.path.exists("pink_morsel.csv"):
+    df_pink.to_csv("pink_morsel.csv", index=False)
 df_pink.to_csv("pink_morsel.csv", index=False)
 
 
