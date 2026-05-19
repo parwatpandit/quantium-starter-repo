5278db5a964bbca688e374f958193519c773f1e8
parwatpandit <parbatp777@gmail.com>
2026-05-19 22:11:26 +0100

    - created task_2.py for task2 and writing code whic generate task_2.csv file according to the requriement of the qeustion

--- task_2.py
+++ task_2.py
@@ -20,31 +20,11 @@ pink_morsel=pd.read_csv("data/pink_morsel.csv")
 
 # removing the $sign form the price so i can calculate the sales
 pink_morsel["price"] = pink_morsel["price"].str.replace("$", "").astype(float)
-# print(pink_morsel["price"])
 
 # calculating sales totoal
 pink_morsel["sales"] = pink_morsel["price"] * pink_morsel["quantity"]
 # print(pink_morsel["sales"])
-print("total Sales:", pink_morsel["sales"].sum())
 
-
-before = pink_morsel[pink_morsel["date"] < "2021-01-15"]
-after = pink_morsel[pink_morsel["date"] >= "2021-01-15"]
-# spliting the sales before and after 15th Jand 2021
-def SalesBeforeAfter():
-    return{
-        "Before:", before["sales"].sum(),
-        "After:", after["sales"].sum()
-    }
-
-SalesBeforeAfter()
-
-
-# outpus the before and after now
-output = pd.DataFrame({
-    "sales": [before["sales"].sum(), after["sales"].sum()],
-    "date": ["before 15th Jan 2021", "after 15th Jan 2021"]
-})
-
-output.to_csv("data/output.csv", index=False)
-print(output)
+# task 2 output format displaying in a new file
+filtered_df = pink_morsel[["sales", "date", "region"]]
+filtered_df.to_csv("data/task_2.csv", index=False)
